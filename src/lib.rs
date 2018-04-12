32269fb6a9ed510517be33dc05cc9b8ac3b807e9
David Rusu <davidrusu.me@gmail.com>
2018-04-11 20:24:59 -0400

    export gitdb modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate toml;
+extern crate ring;
+
+pub mod crypto;
+pub mod encoding;
+pub mod git_creds;
+pub mod git_db;
+pub mod manifest;
+pub mod secret_meta;
+
 #[cfg(test)]
 mod tests {
     #[test]
