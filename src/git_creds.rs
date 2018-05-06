5a3f7a899a8bab29790d54d8722e42dceb76281a
David Rusu <davidrusu.me@gmail.com>
2018-05-06 15:59:17 -0400

    initial stab at writing tests for db.rs; remove old code

--- src/git_creds.rs
+++ src/git_creds.rs
@@ -12,13 +12,15 @@ extern crate git2;
 
 use toml;
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+use db_error::{DBErr};
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct Remotes {
     pub version: String,
     pub remotes: Vec<Remote>
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct Remote {
     pub name: String,
     pub url: String,
@@ -34,14 +36,14 @@ impl Remotes {
         }
     }
     
-    pub fn from_toml_bytes(bytes: &Vec<u8>) -> Result<Remotes, String> {
+    pub fn from_toml_bytes(bytes: &Vec<u8>) -> Result<Remotes, DBErr> {
         toml::from_slice(&bytes)
-            .map_err(|e| format!("Failed to read remotes from TOML: {:?}", e))
+            .map_err(|e| DBErr::Parse(format!("Failed to read remotes from TOML: {:?}", e)))
     }
 
-    pub fn to_toml_bytes(&self) -> Result<Vec<u8>, String> {
+    pub fn to_toml_bytes(&self) -> Result<Vec<u8>, DBErr> {
         toml::to_vec(&self)
-            .map_err(|e| format!("Failed to serialize remotes into TOML: {:?}", e))
+            .map_err(|e| DBErr::Parse(format!("Failed to serialize remotes into TOML: {:?}", e)))
     }
 }
 
