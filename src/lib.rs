1a120d8d53555fce6f1e9a74f7a07c3f945dd7c3
David Rusu <davidrusu@protonmail.com>
2018-08-08 22:00:42 -0400

    A bunch of cruft's been accumulating over the base couple refactors

--- src/lib.rs
+++ src/lib.rs
@@ -12,22 +12,16 @@ extern crate bincode;
 #[macro_use]
 extern crate quickcheck;
 
-pub mod git_helper;
 pub mod error;
 pub mod crypto;
-pub mod encoding;
-pub mod remote;
 pub mod db;
-// pub mod dao;
+pub mod map;
+pub mod data;
 pub mod log;
 pub mod memory_log;
 pub mod git_log;
 pub mod encrypted_git_log;
-pub mod map;
-pub mod data;
 
 pub use error::Error;
 pub use db::DB;
-pub use remote::Remote;
-// pub use dao::Dao;
 pub use log::{LogReplicable, TaggedOp};
